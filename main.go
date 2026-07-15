4e47d455c8d521a854e4b86fb7b5acf734bc0372
Josh <josh_alot@protonmail.com>
2026-07-15 17:00:30 -0300

    integrating the raw mode into the main file

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/terminal_snake_game/internal/input"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	restore, err := input.EnableRawMode()
+	if err != nil {
+		log.Fatalf("failed to enable raw mode: %v", err)
+	}
+	defer restore()
+
+	// ... rest of the game
 }
